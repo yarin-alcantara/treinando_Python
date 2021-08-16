1e4de86d5c81f0678a49fc44f85b808950bd0ee3
Yarin Lemos de Alcantara <yarin_alcantara@hotmail.com>
2021-08-16 20:25:14 -0300

    implementando laço com while

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,23 +3,28 @@ print('Bem vindo ao jogo de adivinhação!')
 print('****************')
 
 numero_secreto = 13
+total_de_tentativas = 3
+rodada = 1
 
-chute = input('Digite o seu numero: ')
+while(rodada <= total_de_tentativas):
+    print('Tentativa {} de {} '.format(rodada, total_de_tentativas))
+    chute_str = input('Digite o seu número: ')
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
 
-print('Você digitou: ', chute_str)
-chute = int(chute_str)
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if (acertou):
-    print('Você acertou')
-else:
-    if(maior):
-        print('Você errou! O seu chute foi maior do que o número secreto')
-    elif(menor):
-        print('Você erro! O seu chute foi menor do que o número secreto')
+    if (acertou):
+        print('Você acertou')
+    else:
+        if(maior):
+            print('Você errou! O seu chute foi maior do que o número secreto')
+        elif(menor):
+            print('Você erro! O seu chute foi menor do que o número secreto')
 
+    rodada = rodada + 1
 print('Fim do jogo')
 print('************')
 
